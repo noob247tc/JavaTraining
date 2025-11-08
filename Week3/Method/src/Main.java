bfe5e1b3fcb273cd9bfb130ed0c28bcd1fd3ac90
Justine Samson <justinesamson5tc@gmail.com>
2025-11-08 17:05:44 +0800

    Update Project

--- Week3/Method/src/Main.java
+++ Week3/Method/src/Main.java
@@ -3,6 +3,34 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println();
+
+
+        int age = 17;
+        if(ageCheck(age)){
+            System.out.println("You may sign up");
+
+        }
+        else{
+            System.out.println("you must be 18");
+        };
+    }
+    static double square(double number){
+        return number * number;
+    }
+
+    static double cube(double number){
+        return number * number;
+    }
+    static String getFullName(String first, String last){
+        return first + " " + last;
+    }
+    static boolean ageCheck(int age){
+        if(age >= 18){
+            return true;
+
+        }
+        else{
+            return false;
+        }
     }
 }
